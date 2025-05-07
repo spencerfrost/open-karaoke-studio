3ad8ecd549eaccb16317bf89394395a8ae84321e
spencerfrost <s.s.frost@gmail.com>
2025-05-07 15:47:23 -0400

    style: Add custom scrollbar styles for .lrc class in App.css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./App.css";
 import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./queryClient";
@@ -10,5 +11,5 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
