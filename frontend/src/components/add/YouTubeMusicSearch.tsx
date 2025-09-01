ce795a33006b34ae5e9c48398cf5b57264e50d1f
spencerfrost <s.s.frost@gmail.com>
2025-09-01 17:11:09 -0400

    feat: remove YouTubeMusicSearch component re-export

--- frontend/src/components/add/YouTubeMusicSearch.tsx
+++
@@ -1,3 +0,0 @@
-// Re-export the refactored YouTubeMusicSearch component
-export { YouTubeMusicSearch } from "./YouTubeMusicSearch/YouTubeMusicSearch";
-export type { YouTubeMusicSearchProps } from "./YouTubeMusicSearch/YouTubeMusicSearch.types";
