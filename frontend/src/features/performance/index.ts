184b836280d274fc41d1a6f8cb457850d6d2dcbc
spencerfrost <s.s.frost@gmail.com>
2026-03-05 21:02:58 -0500

    feat: remove unused PerformanceControlsInput export from performance index

--- frontend/src/features/performance/index.ts
+++ frontend/src/features/performance/index.ts
@@ -5,7 +5,6 @@
 
 // Component exports
 export { default as PerformanceControlsPanel } from "./components/PerformanceControlsPanel";
-export { default as PerformanceControlsInput } from "./components/PerformanceControlsInput";
 export { default as KnobControl } from "./components/KnobControl";
 export { ConnectedPerformanceControls } from "./components/ConnectedPerformanceControls";
 
