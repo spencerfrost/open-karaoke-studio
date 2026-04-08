dc25fbb4cb1a948987f0dc78e05dd2673226700a
spencerfrost <s.s.frost@gmail.com>
2026-04-08 09:26:12 -0400

    chore: delete dead PlayerSidebar component and remove from barrel export

--- frontend/src/features/player/components/subcomponents/index.ts
+++ frontend/src/features/player/components/subcomponents/index.ts
@@ -5,10 +5,6 @@
 export { default as PlayerControls } from "./PlayerControls";
 export { default as FullscreenContainer } from "./FullscreenContainer";
 export { default as PlayerErrorBoundary } from "./PlayerErrorBoundary";
-export {
-  default as PlayerSidebar,
-  PlayerSidebarTrigger,
-} from "./PlayerSidebar";
 export { SettingsMenu, SettingsMenuTrigger } from "./SettingsMenu";
 export { SongEnded } from "./SongEnded";
 export { QueueEnded } from "./QueueEnded";
